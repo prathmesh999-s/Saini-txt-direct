88713ed34c05042e63ef6c87059b4096ecdb55eb
prathmesh999-s <professionalknowledge122@gmail.com>
2025-06-22 12:28:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
+API_ID = int(environ.get("API_ID", "23159825"))
+API_HASH = environ.get("API_HASH", "7a381846fd2b3767c0f7b4f2a52e0f33")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "5680454765"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
